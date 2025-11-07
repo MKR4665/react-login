40055de0e16fd22fde7b1c96f7052cdd0db7831d
MKR4665 <amantusingh4429@gmail.com>
2025-11-07 10:06:53 +0530

    react login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 // import Card from './components/Card'
-// import Login from './components/Login_form/Login'
+import Login from './components/Login_form/Login'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
      <Card />
      <Card />
      <Card /> */}
-      {/* <Login /> */}
+      <Login />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import './components/Login_form/login.css'
-// import App from './App.jsx'
-import Login from './components/Login_form/Login.jsx'
+import App from './App.jsx'
+// import Login from './components/Login_form/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   
-    // <App />
-    <Login />
+    <App />
+    // <Login />
 )
